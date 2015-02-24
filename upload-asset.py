b8ca4cc8c1f78f99a553a668ae5df9d62adc3846
Robin Winslow <robin@robinwinslow.co.uk>
2015-02-24 16:55:17 +0000

    Add ability to specify tags

--- upload-asset.py
+++ upload-asset.py
@@ -27,6 +27,11 @@ parser.add_argument(
     default=os.environ.get('ASSETS_SERVER_TOKEN')
 )
 parser.add_argument(
+    '--tags',
+    help='The URL of the server',
+    default=''
+)
+parser.add_argument(
     '--server-url',
     help='The URL of the server',
     default=os.environ.get('ASSETS_SERVER_URL', 'http://localhost:8080/v1/')
@@ -38,6 +43,7 @@ file_path = cmd_args['file-path']
 url_path = cmd_args['url_path']
 server_url = cmd_args['server_url']
 auth_token = cmd_args['auth_token']
+tags = cmd_args['tags']
 
 asset_mapper = AssetMapper(
     server_url=server_url,
@@ -47,12 +53,14 @@ with open(file_path) as upload_file:
     if url_path:
         response = asset_mapper.create_at_path(
             asset_content=upload_file.read(),
-            url_path=url_path
+            url_path=url_path,
+            tags=tags
         )
     else:
         response = asset_mapper.create(
             asset_content=upload_file.read(),
-            friendly_name=upload_file.name
+            friendly_name=upload_file.name,
+            tags=tags
         )
 
 print response
