8db17af54013b84b42c5a3cc318e64dd7e142dca
Robin Winslow <robin@robinwinslow.co.uk>
2017-04-07 18:31:58 +0100

    Turn this into a python package
    This creates the package `canonicalwebteam.upload_assets`,
    and adds a binary called `upload-assets`.

---
+++ README.rst
@@ -0,0 +1,34 @@
+A command-line tool for managing the `assets server <https://github.com/ubuntudesign/assets-server>`__
+======================================================================================================
+
+This is intended to be a simple command-line tool for uploading assets
+to an https://assets.ubuntu.com-like `assets
+server <https://github.com/ubuntudesign/assets-server>`__.
+
+Installation
+------------
+
+First `install python3
+pip <http://stackoverflow.com/questions/6587507/how-to-install-pip-with-python-3>`__,
+then:
+
+.. code:: bash
+
+    sudo pip3 install canonicalwebteam.upload-assets
+
+Usage
+-----
+
+You should now have access to the ``upload-assets``\ command:
+
+.. code:: bash
+
+    $ upload-assets  \
+        --api-url https://assets.EXAMPLE.com/v1/  \
+        --api-token XXXXXXXX  \
+        ~/EXAMPLE_DIRECTORY ./EXAMPLE_IMAGE.png
+    [
+        {"url": "https://assets.ubuntu.com/v1/2071d161-EXAMPLE_IMAGE.png", "filepath": "/home/robin/EXAMPLE_IMAGE.png"},
+        {"url": "https://assets.ubuntu.com/v1/2071d161-IMAGE1.png", "filepath": "/home/robin/EXAMPLE_DIRECTORY/IMAGE1.png"},
+        {"url": "https://assets.ubuntu.com/v1/2071d161-IMAGE2.png", "filepath": "/home/robin/EXAMPLE_DIRECTORY/IMAGE2.png"}
+    ]
---
+++ canonicalwebteam/upload_assets/uploader.py
@@ -0,0 +1,185 @@
+"""
+Functions for uploading files to an assets.ubuntu.com-like assets server
+"""
+
+# Core packages
+import base64
+import json
+import os
+import glob
+import sys
+
+# Third party packages
+import requests
+
+
+def _upload_file(
+    filepath, api_url, api_token,
+    tags=None, url_path=None, error_on_exists=False
+):
+    """
+    Upload an individual file to an assets.ubuntu.com-like assets server
+    """
+
+    filename = os.path.basename(filepath)
+
+    with open(filepath, 'rb') as asset_file:
+        content = asset_file.read()
+
+    response = requests.post(
+        api_url,
+        data={
+            'asset': base64.b64encode(content),
+            'friendly-name': filename.replace(' ', '+'),
+            'url-path': url_path,
+            'tags': tags,
+            'type': 'base64',
+            'token': api_token
+        }
+    )
+
+    if error_on_exists or response.status_code != 409:
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as request_error:
+            if request_error.response.status_code == 409:
+                if error_on_exists:
+                    print(
+                        "Error: URL path already exists: {url}".format(
+                            url=os.path.join(
+                                api_url,
+                                url_path
+                            )
+                        ),
+                        file=sys.stderr
+                    )
+                    sys.exit(1)
+                else:
+                    pass
+            elif request_error.response.status_code == 403:
+                print(
+                    (
+                        "Error: Permission denied. "
+                        "Did you provide a valid API token?"
+                    ),
+                    file=sys.stderr
+                )
+                sys.exit(1)
+            elif request_error.response.status_code == 502:
+                print(
+                    (
+                        "Error: Bad gateway. Check the API endpoint, "
+                        "e.g. 'https://assets.ubuntu.com/v1/'"
+                    ),
+                    file=sys.stderr
+                )
+                sys.exit(1)
+            else:
+                raise request_error
+
+    asset_info = response.json()
+
+    return {
+        'local_filepath': filepath,
+        'url': os.path.join(
+            api_url,
+            asset_info['file_path']
+        )
+    }
+
+
+def gather_files(paths):
+    """
+    Given a list of valid filepaths from a given set of paths.
+    Links will be followed, and files will be recursively found inside
+    directories
+    """
+
+    filepaths = []
+
+    for path in paths:
+        path = os.path.realpath(path)
+
+        if os.path.isdir(path):
+            path = os.path.join(path, '**')
+        elif not os.path.isfile(path):
+            print(
+                "Warning: '{path}' is not a file".format(**locals()),
+                file=sys.stderr
+            )
+
+        for filepath in glob.glob(path, recursive=True):
+            if os.path.isfile(filepath):
+                filepaths.append(filepath)
+
+    return filepaths
+
+
+def upload_files(filepaths, api_url, api_token, tags=None, url_path=None):
+    """
+    Upload all the provided files to
+    an assets.ubuntu.com-like assets server
+    """
+
+    if len(filepaths) == 0:
+        print('Error: No files specified', file=sys.stderr)
+        sys.exit(1)
+
+    if url_path:
+        if len(filepaths) == 1 and os.path.isfile(filepaths[0]):
+            filepath = filepaths[0]
+        else:
+            print(
+                (
+                    'Error: The "--url-path" option can only '
+                    'be used when uploading a single file'
+                ),
+                file=sys.stderr
+            )
+            sys.exit(1)
+
+        path_filename, path_extension = os.path.splitext(url_path)
+        filename, file_extension = os.path.splitext(filepath)
+
+        if path_extension != file_extension:
+            print(
+                (
+                    "Error: The \"--url-path\"'s extension "
+                    "should match the file type"
+                ),
+                file=sys.stderr
+            )
+            sys.exit(1)
+
+        response = _upload_file(
+            filepath, api_url, api_token, tags, url_path, error_on_exists=True
+        )
+
+        print("[" + json.dumps(response) + "]")
+
+    else:
+        if len(filepaths) == 1:
+            item_info = _upload_file(
+                filepaths[-1], api_url, api_token, tags, url_path
+            )
+            print('[' + json.dumps(item_info) + ']')
+        else:
+            # Do the first item
+            first_item_info = _upload_file(
+                filepaths[0], api_url, api_token, tags, url_path
+            )
+            print('[\n  ' + json.dumps(first_item_info))
+
+            # Do all but the last item
+            for filepath in filepaths[1:-1]:
+                item_info = _upload_file(
+                    filepath, api_url, api_token, tags, url_path
+                )
+                print('  ' + json.dumps(item_info) + ',')
+                sys.stdout.flush()
+
+            # Do the last item
+            last_item_info = _upload_file(
+                filepaths[-1], api_url, api_token, tags, url_path
+            )
+            print('  ' + json.dumps(last_item_info) + '\n]')
